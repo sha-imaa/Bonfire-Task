f3bfe1829ddd4cf8e38c08d98968bead9670a400
Shaimaa <shaimaa.abdelsamad.2015@gmail.com>
2018-07-25 15:06:45 +0100

    introduction slider

---
+++ js/main.js
@@ -0,0 +1,39 @@
+      //change Image slides
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+      //image bubbles control
+function currentSlide(n) {
+    "use strict";
+  slideIndex = n;
+  showSlides();
+}
+
+      
+      //changing images function
+function showSlides(n) {
+    "use strict";
+  var i;
+  var slides = document.getElementsByClassName("mySlides-fade");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length)
+  {
+    slideIndex = 1;
+    }
+  
+  if (n < 1)
+  {
+    slideIndex = slides.length;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+showSlides();
