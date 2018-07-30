c2c7258d534648698b5a900b639949cbe2b22a77
Shaimaa <shaimaa.abdelsamad.2015@gmail.com>
2018-07-30 09:47:10 +0100

    brands slide

--- js/main.js
+++ js/main.js
@@ -37,3 +37,48 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 showSlides();
+
+
+
+
+
+
+      //change Image slides
+var slideIndex = 1;
+changeSlides(slideIndex);
+
+
+
+//buttons' function
+  function plusSlide(n){
+    "use strict";
+  changeSlides(slideIndex = slideIndex + 1);
+  changeSlides();
+  }
+  plusSlide();
+    
+      
+      //changing images function
+function changeSlides(n) {
+    "use strict";
+  var i;
+  var slides = document.getElementsByClassName("brands");
+  if (n > slides.length)
+  {
+    slideIndex = 1;
+    }
+  
+  if (n < 1)
+  {
+    slideIndex = slides.length;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  
+  
+  slides[slideIndex-1].style.display = "block";  
+ 
+}
+changeSlides();
